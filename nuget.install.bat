6757341b412c4235f5a85a8eb1b7c3d357a38c50
wsky <wskyhx@gmail.com>
2012-07-30 16:50:43 +0800

    update nuget feed url

--- nuget.install.bat
+++ nuget.install.bat
@@ -1,3 +1,3 @@
 echo off
-@forfiles /s /m packages.config /c "cmd /c %1\nuget install @file -o %2 -source http://nuget.icodesharp.com/nuget;http://cs-nuget.apphb.com/nuget"
+@forfiles /s /m packages.config /c "cmd /c %1\nuget install @file -o %2 -source https://cs-nuget.apphb.com/nuget"
 echo on
