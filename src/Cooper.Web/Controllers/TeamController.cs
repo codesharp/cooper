cc8ba21cf9fb6b9d1f6ae5249aa425756905247f
wsky <wskyhx@gmail.com>
2012-08-13 17:43:59 +0800

    update contacts module

---
+++ src/Cooper.Web/Controllers/TeamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cooper.Web.Controllers
+{
+    //团队
+    public class TeamController : BaseController
+    {
+
+        public ActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
