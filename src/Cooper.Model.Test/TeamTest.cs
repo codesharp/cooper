5712211120f6c4aecbc560d9f7b3b224870e7b06
tangxuehua <txh_723@163.com>
2012-08-15 18:53:27 +0800

    add team test code

---
+++ src/Cooper.Model.Test/TeamTest.cs
@@ -0,0 +1,83 @@
+﻿//Copyright (c) CodeSharp.  All rights reserved. - http://www.icodesharp.com/
+
+using System;
+using System.Linq;
+using Cooper.Model.Contacts;
+using NUnit.Framework;
+using Cooper.Model.Teams;
+
+namespace Cooper.Model.Test
+{
+    [TestFixture]
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class TeamTest : TestBase
+    {
+        [Test]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void CreateTeam()
+        {
+            var team = new Team("Team 1");
+
+            Assert.AreNotEqual(DateTime.MinValue, team.CreateTime);
+
+            this._teamService.Create(team);
+            Assert.Greater(team.ID, 0);
+
+            this.Evict(team);
+
+            var team2 = this._teamService.GetTeam(team.ID) as Team;
+
+            Assert.AreEqual(team.Name, team2.Name);
+            Assert.AreEqual(FormatTime(team.CreateTime), FormatTime(team2.CreateTime));
+        }
+        [Test]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void UpdateTeam()
+        {
+            var team = new Team("Team 1");
+
+            Assert.AreNotEqual(DateTime.MinValue, team.CreateTime);
+
+            this._teamService.Create(team);
+            Assert.Greater(team.ID, 0);
+
+            this.Evict(team);
+
+            var team2 = this._teamService.GetTeam(team.ID) as Team;
+
+            Assert.AreEqual(team.Name, team2.Name);
+            Assert.AreEqual(FormatTime(team.CreateTime), FormatTime(team2.CreateTime));
+
+            team2.SetName(team.Name + "_updated");
+
+            this._teamService.Update(team2);
+            this.Evict(team2);
+            var team3 = this._teamService.GetTeam(team2.ID) as Team;
+
+            Assert.AreEqual(team2.Name, team3.Name);
+        }
+        [Test]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void DeleteTeam()
+        {
+            var team = new Team("Team 1");
+
+            Assert.AreNotEqual(DateTime.MinValue, team.CreateTime);
+
+            this._teamService.Create(team);
+            Assert.Greater(team.ID, 0);
+
+            this.Evict(team);
+
+            var team2 = this._teamService.GetTeam(team.ID) as Team;
+
+            this._teamService.Delete(team2);
+
+            this.Evict(team2);
+
+            var team3 = this._teamService.GetTeam(team2.ID) as Team;
+
+            Assert.IsNull(team3);
+        }
+    }
+}
