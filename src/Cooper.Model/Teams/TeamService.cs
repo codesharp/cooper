e6400bd96e0035577cf1749dff33700ac717ea64
tangxuehua <txh_723@163.com>
2012-08-22 11:12:05 +0800

    refact team model

--- src/Cooper.Model/Teams/TeamService.cs
+++ src/Cooper.Model/Teams/TeamService.cs
@@ -131,15 +131,13 @@ namespace Cooper.Model.Teams
             Assert.IsValidKey(name);
             Assert.IsValidKey(email);
             Assert.IsValid(team);
-            var member = new Member(name, email, team);
-            member.Associate(account);
 
             //HACK:由于此时在事务中，并且member可能被更新，此时的查询会导致nh提供事务因此应该先查询再AddMember
             //这样做可以确保数据库所有的Member的Email唯一
             this._locker.Require<Member>();
-            Assert.IsNull(_teamRepository.FindMemberBy(team, member.Email));
+            Assert.IsNull(_teamRepository.FindMemberBy(team, email));
 
-            team.AddMember(member);
+            var member = team.AddMember(name, email, account);
             _teamRepository.Update(team);
 
             return member;
@@ -167,9 +165,10 @@ namespace Cooper.Model.Teams
         {
             Assert.IsValidKey(name);
             Assert.IsValid(team);
-            var project = new Project(name, team);
-            team.AddProject(project);
+
+            var project = team.AddProject(name);
             _teamRepository.Update(team);
+
             return project;
         }
         [Transaction(TransactionMode.Requires)]
