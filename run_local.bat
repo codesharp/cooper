069bc7456dffa606055f4055fe7a8f4c2cb8bc31
wsky <wskyhx@gmail.com>
2012-09-04 11:17:44 +0800

    add ie start

--- run_local.bat
+++ run_local.bat
@@ -2,4 +2,5 @@
 echo Run on IISExpress
 echo Prepending 'C:\PROGRA~2\IIS Express' to PATH
 PATH=C:\PROGRA~2\IIS Express;%PATH%
+start iexplore "http://localhost:8889"
 iisexpress.exe /path:%~dp0src\Cooper.Web  /port:8889 /clr:V4.0
