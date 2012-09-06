a69e094be05cca3ff17375775d7d000ccfb41b09
wsky <wskyhx@gmail.com>
2012-09-07 00:20:54 +0800

    update nuget.sh

--- nuget.install.sh
+++ nuget.install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf packages
 mono --runtime=v4.0 ../work-tool/NuGet.exe install src/Cooper.Model/packages.config -o packages -NoCache -source http://cs-nuget.apphb.com/nuget;http://nuget.org/api/v2/
 mono --runtime=v4.0 ../work-tool/NuGet.exe install src/Cooper.Model.Mappings/packages.config -o packages -NoCache -source http://cs-nuget.apphb.com/nuget;http://nuget.org/api/v2/
 mono --runtime=v4.0 ../work-tool/NuGet.exe install src/Cooper.Model.Test/packages.config -o packages -NoCache -source http://cs-nuget.apphb.com/nuget;http://nuget.org/api/v2/
