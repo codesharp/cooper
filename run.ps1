5215d1049f17fd1facfe28c79ae5d3ae9883c44b
wsky <wskyhx@gmail.com>
2012-09-07 14:37:43 +0800

    update run

--- run.ps1
+++ run.ps1
@@ -1,18 +1,11 @@
-.\build web Debug
+.\build web Test
 
 $dir=Get-Location
 $dir=''+$dir
 
-$p=$dir+'\build\debug\cooper_web'
+$p=$dir+'\build\test\cooper_web'
 echo 'Run on IISExpress'
 $env:Path=$env:Path+';C:\PROGRA~2\IIS Express'
 start iexplore "http://localhost:8889"
 iisexpress.exe /path:$p  /port:8889 /clr:V4.0
-
-if($false){
-$p_mono=$dir+'\build\debug\cooper_web_mono'
-echo 'Run on MONO'
-$env:Path=$env:Path+';C:\PROGRA~2\MONO-2~1.8\bin'
-xsp4 --port 8889 --root $p_mono
-}
 
