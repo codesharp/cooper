5babb8f81bba097cff8a0ac2d5b3cf2e57813021
wsky <wskyhx@gmail.com>
2012-09-07 16:43:26 +0800

    update script

--- nuget.install.bat
+++ nuget.install.bat
@@ -1,3 +1,4 @@
+del packages /F /S /Q
 echo off
-@forfiles /s /m packages.config /c "cmd /c %1\nuget install @file -o %2 -source http://cs-nuget.apphb.com/nuget;http://nuget.org/api/v2/"
+@forfiles /s /m packages.config /c "cmd /c %1\nuget install @file -o %2 -NoCache -source http://cs-nuget.apphb.com/nuget;http://nuget.org/api/v2/"
 echo on
