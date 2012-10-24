7d66c5824e376acb1eb76e8b0ccdc092eebcfa79
xuehua <txh_723@163.com>
2012-10-24 17:27:04 +0800

    add a repository to cache textsnippet.

---
+++ src/Cooper.TextSnippetService/NHibernateTextSnippetRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Castle.Facilities.NHibernateIntegration;
+using Castle.Services.Transaction;
+using NHibernate.Criterion;
+
+namespace Cooper.TextSnippetService
+{
+    [Transactional]
+    public class NHibernateTextSnippetRepository : ITextSnippetRepository
+    {
+        private const string GetTextSnippetSQL = "select Id, Type, Key, SnippetText from Cooper_TextSnippet where Type=:Type and Key=:key";
+        private const string AddTextSnippetSQL = "insert into Cooper_TextSnippet values (:Id, :Type, :Key, :SnippetText)";
+        private const string RemoveTextSnippetSQL = "delete from Cooper_TextSnippet where Id=:Id";
+
+        private ISessionManager _sessionManager;
+
+        public NHibernateTextSnippetRepository(ISessionManager sessionManager)
+        {
+            _sessionManager = sessionManager;
+        }
+
+        [Transaction]
+        public TextSnippet GetTextSnippet(TextSnippetType type, string key)
+        {
+            var query = _sessionManager.OpenSession().CreateSQLQuery(GetTextSnippetSQL);
+            query.SetEnum("Type", type);
+            query.SetString("Key", key);
+            var result = query.UniqueResult() as object[];
+
+            if (result != null)
+            {
+                return new TextSnippet
+                {
+                    Id = new Guid(result[0].ToString()),
+                    Type = (TextSnippetType)Enum.Parse(typeof(TextSnippetType), result[1].ToString()),
+                    Key = result[2].ToString(),
+                    SnippetText = result[3] as string
+                };
+            }
+
+            return null;
+        }
+        [Transaction]
+        public Guid AddTextSnippet(TextSnippetType type, string key, string snippetText)
+        {
+            var id = Guid.NewGuid();
+            var query = _sessionManager.OpenSession().CreateSQLQuery(AddTextSnippetSQL);
+            query.SetGuid("Id", id);
+            query.SetEnum("Type", type);
+            query.SetString("Key", key);
+            query.SetString("SnippetText", snippetText);
+            query.ExecuteUpdate();
+            return id;
+        }
+        [Transaction]
+        public void RemoveTextSnippet(Guid id)
+        {
+            var query = _sessionManager.OpenSession().CreateSQLQuery(RemoveTextSnippetSQL);
+            query.SetGuid("Id", id);
+            query.ExecuteUpdate();
+        }
+    }
+}
---
+++ src/Cooper.TextSnippetService/TextSnippet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cooper.TextSnippetService
+{
+    /// <summary>
+    /// 表示一条文本缩略信息
+    /// </summary>
+    public class TextSnippet
+    {
+        /// <summary>
+        /// 唯一表示，自动生成
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 一个Key，可以为一个URL，或者一个evernote笔记的ID，等等
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// 文本缩略信息的类型，比如html, evernote
+        /// </summary>
+        public TextSnippetType Type { get; set; }
+        /// <summary>
+        /// 文本缩略信息
+        /// </summary>
+        public string SnippetText { get; set; }
+    }
+    public enum TextSnippetType
+    {
+        /// <summary>
+        /// Html页面的纯文本缩略信息
+        /// </summary>
+        Html,
+        /// <summary>
+        /// EverNote笔记的纯文本缩略信息
+        /// </summary>
+        EverNote
+    }
+}
